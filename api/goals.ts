aa1a0592693a565dae620107101c0eb26beaf160
Krish Raja <hello@krishraja.com>
2026-04-22 21:34:24 +0000

    Trigger redeploy

--- api/goals.ts
+++ api/goals.ts
@@ -80,3 +80,4 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   return res.status(405).json({ error: 'Method not allowed' })
 }
+// deploy trigger Wed Apr 22 09:34:24 PM UTC 2026
